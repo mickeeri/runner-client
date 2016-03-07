53afdae6cf2596d780277e25422efd85f5608d1d
Mikael Eriksson <me222wm@student.lnu.se>
2016-03-07 21:35:32 +0000

    Started on service methods.

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -1,6 +1,8 @@
-angular
-    .module("raceApp", ['ngRoute', 'LocalStorageModule'])
-    .config(['$routeProvider', '$locationProvider',
+'use strict';
+/*global angular*/
+
+angular.module("raceApp", ['ngRoute', 'LocalStorageModule']).
+    config(['$routeProvider', '$locationProvider',
     function($routeProvider, $locationProvider) {
       $routeProvider.
         when('/', {
@@ -15,4 +17,20 @@ angular
           redirectTo: '/'
         });
       $locationProvider.html5Mode(true); // This removes the hash-bang and use the Session history management >= IE10
-    }]);
+    }])
+    .config(function (localStorageServiceProvider) {
+      // The module give me some stuff to configure
+      localStorageServiceProvider
+          .setPrefix('raceApp')
+          .setStorageType('sessionStorage')
+          .setNotify(true, true)
+    })
+    .constant('API', { 
+      'key': "cf46dd8a63811111469ea022d320f51f", 
+      'url': "http://localhost:3000/api/v1/", 
+      'format': 'application/json' 
+    })
+    .constant('LocalStorageConstants', {
+      'racesKey' : 'r', 
+      'locationsTeam'   : 'l'
+    });
